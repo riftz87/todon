54b51f81fe47c6fa7f3cc964e12d89a46302bbea
riftz87 <riftz.dev@gmail.com>
2022-05-06 17:03:53 +0800

    Added toast messages

--- components/Form.vue
+++ components/Form.vue
@@ -23,6 +23,30 @@
             <Button label="Discard" icon="pi pi-trash" class="p-button-text p-button-danger" @click="handleDiscard" />
             <Button label="Save" icon="pi pi-save" @click="handleSave" />
         </div>
+        <div>
+            <Toast position="center" group="bc">
+                <template #message="slotProps">
+                    <div class="flex flex-col w-full">
+                        <div class="flex">
+                            <div class="p-5">
+                                <i :class="slotProps.message.mode == 'probe' ? 'pi pi-exclamation-triangle' : 'pi pi-check-circle'" style="font-size: 3rem"></i>
+                            </div>
+                            <div class="flex-grow flex flex-col justify-center px-3 py-5">
+                                <h4>{{ slotProps.message.summary }}</h4>
+                                <p>{{ slotProps.message.detail }}</p>
+                            </div>
+                        </div>
+                        <div v-if="slotProps.message.mode == 'probe'" class="text-right pt-5">
+                            <Button class="p-button-success" label="Yes" @click="onConfirmDiscard"></Button>
+                            <Button class="p-button-secondary" label="No" @click="onRejectDiscard"></Button>
+                        </div>
+                        <div v-else class="text-right pt-5">
+                            <Button class="p-button-success" label="OK" @click="onConfirm(slotProps)"></Button>
+                        </div>
+                    </div>
+                </template>
+            </Toast>
+        </div>
     </div>
 </template>
 
@@ -32,6 +56,7 @@ import clonedeep from 'lodash/cloneDeep';
 export default {
     data() {
         return {
+            success: false,
             item: {
                 key: Math.random().toString(36).substring(2, 7),
                 title: null,
@@ -70,13 +95,26 @@ export default {
         },
         handleDiscard() {
             const e = this;
+            e.$toast.add({mode: 'probe', severity: 'warn', summary: 'Are you sure you want to discard this list?', detail: 'Proceed to confirm', group: 'bc', styleClass: '!border-none', contentStyleClass: '!border-none'});
+        },
+        onConfirmDiscard() {
+            const e = this;
             const { item } = e;
             e.$emit('discard', item);
+        },
+        onRejectDiscard() {
+            const e = this;
+            e.$toast.removeGroup('bc');
+        },
+        onConfirm(props) {
+            const e = this;
+            e.$toast.removeGroup('bc');
         },
         handleSave() {
             const e = this;
             const { item } = e;
             e.$emit('save', item);
+            e.$toast.add({mode: 'prompt', severity: 'success', summary: 'TODO list has been saved.', detail: '', group: 'bc', styleClass: '!border-none', contentStyleClass: '!border-none'});
         }
     }
 }
--- plugins/UIKit.js
+++ plugins/UIKit.js
@@ -3,8 +3,14 @@ import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import ToggleButton from 'primevue/togglebutton';
 import Skeleton from 'primevue/skeleton';
+import Message from 'primevue/message';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
 
+Vue.use(ToastService);
 Vue.component('Button', Button);
 Vue.component('InputText', InputText);
 Vue.component('ToggleButton', ToggleButton);
 Vue.component('Skeleton', Skeleton);
+Vue.component('Message', Message);
+Vue.component('Toast', Toast);
